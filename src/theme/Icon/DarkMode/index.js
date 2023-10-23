e0cbdfbd22c0295242dacf522ec26990e889d7d6
Adventurer Monthly <adventurermonthly@gmail.com>
2023-10-22 21:05:31 -0700

    candle icon for light and dark toggle

---
+++ src/theme/Icon/DarkMode/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import CandleDark from '/svg/candle-dark.svg'
+export default function IconDarkMode(props) {
+  return (
+    <CandleDark {...props} />
+  );
+}
---
+++ src/theme/Icon/LightMode/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import CandleLight from '/svg/candle-light.svg'
+export default function IconLightMode(props) {
+  return (
+      <CandleLight {...props} />
+  );
+}
